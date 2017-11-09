11fb7d686e737d9c5e9d4a70b8233cc49dd9aa1c
changlz <changlizhi@aliyun.com>
2017-11-09 17:39:35 +0800

    fmt

--- src/components/csshouye/Csshouye.vue
+++ src/components/csshouye/Csshouye.vue
@@ -1,29 +1,42 @@
 <template>
-  <div>
-    <ul class='daohang'>
-      <li><a v-link="{path:'/usetabs'}"
-           style='font-size: 22px'>usetabs
-        </a></li>
-      <li><a v-link="{path:'/useslider'}"
-           style='font-size: 22px'>useslider
-        </a></li>
-      <li><a v-link="{path:'/usenavbar'}"
-           style='font-size: 22px'>usenavbar
-        </a></li>
-    </ul>
-  </div>
+    <div>
+        <ul class='daohang'>
+            <li>
+                <a v-link="{path:'/usetabs'}"
+                   style='font-size: 22px'>usetabs
+                </a>
+            </li>
+            <li>
+                <a v-link="{path:'/useslider'}"
+                   style='font-size: 22px'>useslider
+                </a>
+            </li>
+            <li>
+                <a v-link="{path:'/usenavbar'}"
+                   style='font-size: 22px'>usenavbar
+                </a>
+            </li>
+            <li>
+                <a v-link="{path:'/usealert'}"
+                   style='font-size: 22px'>usealert
+                </a>
+            </li>
+        </ul>
+    </div>
 </template>
 <script>
   export default{}
 </script>
 <style scoped>
-  .daohang {
-    padding: 30px 20px;
-  }
-  .daohang li {
-    line-height: 30px;
-  }
-  .daohang a:hover {
-    text-decoration: none;
-  }
+    .daohang {
+        padding: 30px 20px;
+    }
+
+    .daohang li {
+        line-height: 30px;
+    }
+
+    .daohang a:hover {
+        text-decoration: none;
+    }
 </style>
